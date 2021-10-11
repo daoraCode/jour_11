0c0d652dd6802538c50b2037d1dd4ade39f4c22b
daoraCode <severin.mboukou@gmail.com>
2021-10-11 14:52:07 +0200

    Exercice 2 et 3 terminés

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 // Exercice 1 - File System
-var fs = require('fs');
-console.log(fs);
+// var fs = require('fs');
+// console.log(fs);
+// fs.readFile('jour07.txt', function (err, data) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('Lecture en différé : ' + data.toString());
+// });
 
-fs.readFile('jour07.txt', function (err, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+// fs.unlink('jour07.txt', function (err) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('jour07.txt à bien été supprimé');
+// });
 
-  console.log('Lecture en différé : ' + data.toString());
-});
+// Exercice 02 - Map Double
+//  var array = [1, 2, 3, 4, 5];
 
-fs.unlink('jour07.txt', function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+//  var double = array.map(function (number) {
+//     return number * 2
+//  });
+//  console.log(double); // [ 2, 4, 6, 8, 10 ]
 
-  console.log('jour07.txt à bien été supprimé');
-});
+// Exercice 03 - Map Names
+// var longNames = [
+//   { firstName: 'Jane', lastName: 'Doe' },
+//   { firstName: 'John', lastName: 'Smith' },
+// ];
+// var shortNames = longNames.map(function (shortName) {
+//     return `${shortName.firstName} ${shortName.lastName}`
+// })
+// console.log(shortNames);
 
 
+
+
+
+
+
+
+// Exercice 04 - Filter Numbers
+
+var array = [1, "toto", 34, "javaScript"]
