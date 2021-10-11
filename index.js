7ea47bf4c9beb71c85914512cb76cd4d2b50bf9d
daoraCode <severin.mboukou@gmail.com>
2021-10-11 15:50:33 +0200

    Exercice 4, 5, 6 globalement achevés

--- index.js
+++ index.js
@@ -7,8 +7,7 @@
 //     return;
 //   }
 //   console.log('Lecture en différé : ' + data.toString());
-// });
-
+// })
 // fs.unlink('jour07.txt', function (err) {
 //   if (err) {
 //     console.log(err);
@@ -16,6 +15,8 @@
 //   }
 //   console.log('jour07.txt à bien été supprimé');
 // });
+// console.log('Exercice 1');
+
 
 // Exercice 02 - Map Double
 //  var array = [1, 2, 3, 4, 5];
@@ -24,6 +25,7 @@
 //     return number * 2
 //  });
 //  console.log(double); // [ 2, 4, 6, 8, 10 ]
+// console.log('Exercice 2')
 
 // Exercice 03 - Map Names
 // var longNames = [
@@ -31,17 +33,66 @@
 //   { firstName: 'John', lastName: 'Smith' },
 // ];
 // var shortNames = longNames.map(function (shortName) {
-//     return `${shortName.firstName} ${shortName.lastName}`
-// })
+//   return shortName.firstName + ' ' + shortName.lastName;
+// });
+// console.log('Exercice 3');
 // console.log(shortNames);
 
+// // Exercice 04 - Filter Numbers
+var array = [1, "toto", 34, "javaScript"];
+var numbers = array.filter(function (num) {
+    if(num === typeof Number) {
+        return typeof num;
+    } else {
+        return "";
+    }
+});
+console.log('Exercice 4')
+console.log(numbers);
 
+// Exercice 05 - Filter Even
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
+var even = numbers.filter(function (ev) {
+    return ev % 2 === 0;
+});
+console.log('Exercice 5')
+console.log(even);
 
-
-
-
-
-// Exercice 04 - Filter Numbers
-
-var array = [1, "toto", 34, "javaScript"]
+// Exercice 06 - Cakes
+var cakes = [
+  {
+    name: 'cake',
+    flavor: 'vanilla',
+    status: 'available',
+  },
+  {
+    name: 'brownie',
+    flavor: 'chocolate',
+    status: 'available',
+  },
+  {
+    name: 'pie',
+    flavor: 'strawberry',
+    status: 'available',
+  },
+  {
+    name: 'muffin',
+    flavor: 'pistachio',
+    status: 'available',
+  },
+  {
+    name: 'donut',
+    flavor: 'chocolate',
+    status: 'available',
+  },
+];
+// je crée la variable des chocolats en rupture de vente
+var soldOutCakes = cakes.filter(function (chocolateCake) {
+  if (chocolateCake.flavor === 'chocolate') {
+    chocolateCake.status = "sold out !"
+    return chocolateCake.name + chocolateCake.flavor + chocolateCake.status;
+  }
+});
+console.log('Exercice 6');
+console.log(soldOutCakes);
